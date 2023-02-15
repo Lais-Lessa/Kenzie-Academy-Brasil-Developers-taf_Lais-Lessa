cd62643c97819d4384052a80732144254c63bf93
Lais Lessa <124070111+Lais-Lessa@users.noreply.github.com>
2023-02-15 14:23:45 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Variáveis para aplicar os resultados do teste. Não as altere.
+
+gender = process.argv[2]; // Sexo 
+height = process.argv[3]; // Altura
+barReps = process.argv[4]; // Repetições com barra
+barSeconds = process.argv[5]; // Tempo das repetições com barra 
+abs = process.argv[6]; // Abdominais
+runDistance = process.argv[7]; // Distância da corrida
+runTime = process.argv[8]; // Tempo da corrida
+swimDistance = process.argv[9]; // Distância da natação
+swimTime = process.argv[10]; // Tempo da natação
+diveTime = process.argv[11]; // Tempo de mergulho
+
+passed = false;
+
+// Importante:
+// Se o sexo (gender) for masculino, utilize a palavra "male"
+// Se o sexo (gender) for feminino, utilize a palavra "female"
+
+// Sugestão para começar:
+// if (gender === "male")  
+// **sua lógica a partir daqui**
+
+
+
+
+// Deve conter apenas esse console.log no seu código.
+// Senão os testes não irão funcionar.
+console.log(passed);
